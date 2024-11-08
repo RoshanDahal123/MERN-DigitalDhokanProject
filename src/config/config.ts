16e53aaabebaee6c5ae27e89291a64253c66845b
RoshanDahal123 <dahalroshan765@gmail.com>
2024-11-08 12:43:18 +0545

    database connection (postgress)

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,4 +2,5 @@ import { configDotenv } from "dotenv";
 configDotenv();
 export const envConfig = {
   port: process.env.PORT,
+  connectionString: process.env.CONNECTION_STRING,
 };
---
+++ src/database/connection.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize-typescript";
+import { envConfig } from "../config/config";
+const sequelize = new Sequelize(envConfig.connectionString as string);
+try {
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("milyo hai authentication!!");
+    })
+    .catch((err) => {
+      console.log("error aayo", err);
+    });
+} catch (error) {
+  console.log(error);
+}
+export default sequelize;
