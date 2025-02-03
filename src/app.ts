0fb43bfd150b70ec2e580ba0aaa7524724dc1ecf
RoshanDahal123 <dahalroshan765@gmail.com>
2025-02-03 23:59:34 +0545

    cors fixed

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,12 @@ import categoryRoute from "./routes/categoryRoute";
 import productRoute from "./routes/productRoute";
 import orderRoute from "./routes/orderRoute";
 import cartRoute from "./routes/cartRoute";
-
+import cors from "cors";
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", userRoute);
